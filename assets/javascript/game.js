e5a8f0301b5311664d56159dba3697673404ca4c
Joe Wright <joeshome@Joes-MacBook.local>
2018-03-05 17:42:15 -0500

    got a ton of JS done today, that plus some html and css updates as well all cosmetic and adding needed ids for js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,2 +1,147 @@
 // Javascript
 
+//DOM Elements
+var $newGameButton = document.getElementById("newGameButton");
+var $artImg = document.getElementById("artImg");
+var $placeholders = document.getElementById("placeholders");
+var $guessed = document.getElementById("guessed");
+var $guessesLeft = document.getElementById("guessesLeft")
+var $vanGoghImg = document.getElementById("vanGoghImg");
+var $vanGoghSays = document.getElementById("vanGoghSays");
+var $hangManSays = document.getElementById("hangManSays");
+var $hintBox = document.getElementById("hintBox");
+var $wins = document.getElementById("wins");
+var $losses = document.getElementById("losses");
+
+//variables
+var wordBank = ["Basquiat", "Monet", "Munch", "Max Beckmann", "Andy Warhol", "Jackson Pollock", "Kehinde Wiley", "Picasso", "Dali"]
+var wins = 0;
+var losses = 0;
+var guessesLeft = 7;
+var gameRunning = false;
+var pickedWord = "";
+var selecteddWordArr = [];
+var guessedBank = [];
+var wrongBank = [];
+var vanGoghSays = "I'm going to do it, I'm going to cut off my ear for love"
+
+//art image functionality
+
+
+//newgame function
+function newGame() {
+  gameRunning = true;
+  guessesLeft = 7;
+  guessedBank = [];
+  wrongBank = [];
+  selecteddWordArr = [];
+  pickedWord = wordBank[Math.floor(Math.random() * wordBank.length)];
+    for (var i = 0; i < pickedWord.length; i++) {
+      if (pickedWord[i] === " ") {
+        selecteddWordArr.push(" ");
+        }
+      else {
+        selecteddWordArr.push("-");
+      }
+    }
+  $vanGoghImg.src = "./assets/images/VanGogh7.jpg"
+  $guessesLeft.textContent = guessesLeft;
+  $placeholders.textContent = selecteddWordArr.join("");
+  $guessed.textContent = wrongBank;
+  $vanGoghSays.textContent = "I'm going to do it, I'm going to cut off my ear for love";
+}
+
+//letter guess function
+function letterGuess(letter) {
+  if (gameRunning === true && guessedBank.indexOf(letter) === -1) {
+    guessedBank.push(letter);
+    for (var i = 0; i < pickedWord.length; i++) {
+      if (pickedWord[i].toLowerCase() === letter.toLowerCase()) {
+        selecteddWordArr[i] = pickedWord[i];
+         $hangManSays.textContent = "The Game Says: Good Job!";
+      }
+    }
+    $placeholders.textContent = selecteddWordArr.join('')
+    letterWrong(letter)
+  }
+  else {
+    if (gameRunning === false) {
+      $hangManSays.textContent = "The Game Says: Press the button below to start a game!";
+    }
+    else {
+      $hangManSays.textContent = "The Game Says: You've already guessed that letter!";
+    } 
+  }
+}
+
+//check incorrect
+function letterWrong(letter) {
+  if (selecteddWordArr.indexOf(letter.toLowerCase()) === -1 && selecteddWordArr.indexOf(letter.toUpperCase()) === -1) {
+    guessesLeft--;
+    wrongBank.push(letter);
+    $guessed.textContent = wrongBank.join(" ");
+    $guessesLeft.textContent = guessesLeft;
+    $hangManSays.textContent = "The Game Says: Too Bad; You're Wrong!"
+    if (guessesLeft === 6) {
+      $vanGoghImg.src = "./assets/images/VanGogh6.jpg";
+      $vanGoghSays.textContent = "Van Gogh Says: Ouch well that smarts a bit";
+    }
+    else if (guessesLeft === 5) {
+      $vanGoghImg.src = "./assets/images/VanGogh5.jpg";
+      $vanGoghSays.textContent = "Van Gogh Says: Oh boy, really feeling it now";
+    }
+    else if (guessesLeft === 4) {
+      $vanGoghImg.src = "./assets/images/VanGogh4.jpg";
+      $vanGoghSays.textContent = "Van Gogh Says: Could you please mind your guesses this hurts!";
+    }
+    else if (guessesLeft === 3) {
+      $vanGoghImg.src = "./assets/images/VanGogh3.jpg";
+      $vanGoghSays.textContent = "Van Gogh Says: Oh Nooo, I think I've made a mistake";
+    }
+    else if (guessesLeft === 2) {
+      $vanGoghImg.src = "./assets/images/VanGogh2.jpg";
+      $vanGoghSays.textContent = "Van Gogh Says: REALLY REALLY FEELING IT NOW OUCH OUCH";
+    }
+    else if (guessesLeft === 1) {
+      $vanGoghImg.src = "./assets/images/VanGogh1.jpg";
+      $vanGoghSays.textContent = "Van Gogh Says: OH FOR GODS SAKE JUST GET IT OFF ALREADY";
+    }
+  }
+  checkLose() ;
+}
+
+function checkLose() {
+  if (guessesLeft === 0) {
+    console.log("you lose")
+    losses++;
+    gameRunning= false;
+    $losses.textContent = losses;
+    $vanGoghImg.src = "./assets/images/VanGogh.jpg";
+    $vanGoghSays.textContent = "I SHOULD HAVE JUST GOTTEN HER NAME TATTOOED ON ME LIKE MY MATE SAID THIS WAS STUPID";
+    $hangManSays.textContent  = "You Lose, But Try Again! Press the Start A New Game button below";
+  }
+  checkWin() ;
+}
+function checkWin() {
+  if (pickedWord.toLowerCase() === selecteddWordArr.join().toLowerCase) {
+    console.log("you win")
+    wins++;
+    gameRunning: false;
+    $wins.textContent = wins;
+    $vanGoghImg.src = "./assets/images/VanGogh.jpg";
+    $vanGoghSays.textContent = "Ugh, I can't believe I started to cut my ear off and can't even go through with it, so lame";
+    $hangManSays.textContent  = "You Win; Press Start A New Game to Play Again!";
+  }
+}
+
+$newGameButton.addEventListener('click', newGame)
+
+document.onkeyup = function(e) {
+  if(e.keyCode >=65 && e.keyCode <= 90) {
+    letterGuess(e.key);
+  }
+  else {
+    $hangManSays.textContent = "That isn't a letter!";
+  }
+}
+
